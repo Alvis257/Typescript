f84e92ff912dd3f5a93b6378fa032c2aa7a619c6
Alvis257 <90256392+Alvis257@users.noreply.github.com>
2021-09-28 00:39:17 +0300

    Add files via upload

---
+++ src/00-string-to-array/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Write a function that converts the given string to an array of it's characters.
+ * Return the result.
+ * 
+ * Example
+ * 
+ * Input: "Kitten"
+ * Output: ["K","i","t","t","e","n"]
+ */
+
+function stringToArray(input: string):string[] {
+    let newString = input.split("");
+    return newString;
+}
+
+export { stringToArray };
