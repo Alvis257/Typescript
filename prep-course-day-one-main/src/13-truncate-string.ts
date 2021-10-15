78a56c1917b6a5649af6ed0c80bd9c4185938253
Alvis257 <90256392+Alvis257@users.noreply.github.com>
2021-10-15 20:41:50 +0300

    Delete prep-course-day-one-main/src directory

--- prep-course-day-one-main/src/13-truncate-string.ts
+++
@@ -1,7 +0,0 @@
-import { sortAndDeduplicateDiagnostics } from "typescript";
-
-export { };
-function truncateString(word: string, number: number): any {
-        return word.slice(0, number);
-}
-console.log(truncateString("CODELEX", 4)); // Expected output: CODE
--- prep-course-day-one-main/src/27-full-name.ts
+++
@@ -1,9 +0,0 @@
-export { };
-
-/**
- * Create a function called fullName, which concatenates two strings together
- */
-function fullName(name1: string, name2: string): string {
-    return "'" + name1.concat(' ').concat(name2) + "'";
-}
-console.log(fullName("John", "Doe")); //Expected output: 'John Doe'
